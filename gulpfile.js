8e379fcfdd98f196fa0ed73b32f0f91f49f4d5ba
Linus Miller <lohfu@lohfu.io>
2014-11-08 13:32:03 +0100

    Large restructure and implementation of Gulp instead of Grunt. Mainly all default SCSS moves into sass/base/

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var ignore = require('gulp-ignore');
+var rimraf = require('rimraf');
 
 gulp.task('default', function() {
-	gulp.src('./sass/variables/**/*.scss').
-	 pipe(concat('_variables_spysass.scss')).
-	 pipe(gulp.dest('./sass/variables/'));
+
+	gulp.src(['./sass/variables/**/*.scss', '!**/themes/**', '!**/_variables_spysass.scss'])
+	 //.pipe(ignore('**/themes/**', '**/_variables_spysass.scss'))
+	 .pipe(concat('_variables_spysass.scss'))
+	 .pipe(gulp.dest('./sass/variables/'));
 });
